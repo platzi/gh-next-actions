24587b4072fa629b271e1bd989ea2b3632905de0
devferx <ferkki@hotmail.com>
2022-08-29 11:49:02 -0400

    :fire: [INTRODUCE] Lint errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
-import 'tailwindcss/tailwind.css';
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
+
+
+
+
+
   return <Component {...pageProps} />;
 }
+
+
+
 
 export default MyApp;
