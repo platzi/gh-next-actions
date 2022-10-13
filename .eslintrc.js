cae01b35bb433c2b8d8c4c3a4fc1144b23ee15d6
WilliamAbelCondori <1williamcondori1@gmail.com>
2022-10-13 05:47:49 -0400

    create workflow test

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,14 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
-    'next',
-    'next/core-web-vitals',
-  ],
+  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
   rules: {
-    'semi': ['error', 'always'],
+    semi: ['error', 'always'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
-}
+};
